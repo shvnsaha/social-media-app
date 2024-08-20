adcfe48ebc36ba8128a0b71b56238f26fb2bd7e8
shvnsaha <shovan.css2018@gmail.com>
2024-08-20 11:17:35 +0600

    user controller done

---
+++ server/controllers/user.controller.js
@@ -0,0 +1,202 @@
+import { User } from "../models/user.model";
+import bcrypt from 'bcryptjs'
+import jwt from 'jsonwebtoken'
+import getDataUri from "../utils/datauri";
+import cloudinary from "../utils/cloudinary";
+
+
+export const register = async (req, res) => {
+    try {
+        const { username, email, password } = req.body;
+        if (!username || !email || !password) {
+            return res.status(401).json({
+                message: "Something Went Wrong",
+                success: false
+            })
+        }
+
+        const user = await User.findOne({ email })
+        if (user) {
+            return res.status(401).json({
+                message: "Email address already exists",
+                success: false
+            })
+        }
+
+        const hashedPassword = await bcrypt.hash(password, 10);
+
+        await User.create({
+            username,
+            email,
+            password: hashedPassword
+        })
+
+        return res.status(401).json({
+            message: "Account Created successfully",
+            success: true
+        })
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+export const login = async (req, res) => {
+    try {
+        const { email, password } = req.body;
+        if (!email || !password) {
+            return res.status(401).json({
+                message: "Something Went Wrong",
+                success: false
+            })
+        }
+
+        let user = await User.findOne({ email })
+        const isPassworMatch = await bcrypt.compare(password, user.password);
+        if (!user || !isPassworMatch) {
+            return res.status(401).json({
+                message: "Incorrect Email or password",
+                success: false
+            })
+        }
+
+        const token = await jwt.sign({ userId: user._id }, process.env.SECRET_KEY, { expiresIn: '1d' });
+
+        user = {
+            _id: user._id,
+            username: user.username,
+            email: user.email,
+            profilePicture: user.profilePicture,
+            bio: user.bio,
+            followers: user.followers,
+            following: user.following,
+            posts: user.posts
+        }
+        return res.cookie('token', token, { httpOnly: true, sameSite: 'strict', maxAge: 1 * 24 * 60 * 60 * 1000 }).json({
+            message: `Welcome Back ${user.username}`,
+            success: true,
+            user
+        })
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+export const logout = async (req, res) => {
+    try {
+        return res.cookie("token", "", { maxAge: 0 }).json({
+            message: 'Logged out successfully',
+            success: true
+        })
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+export const getProfile = async (req, res) => {
+    try {
+        const userId = req.params.id;
+        let user = await User.findById(userId)
+    } catch (error) {
+
+    }
+}
+
+export const editProfile = async (req, res) => {
+    try {
+        const userId = req.id;
+        const { bio, gender } = req.body;
+        const profilePicture = req.file;
+        let cloudResponse;
+        if (profilePicture) {
+            const fileUri = getDataUri(profilePicture);
+            cloudResponse = await cloudinary.uploader.upload(fileUri)
+        }
+        const user = await User.findById(userId)
+        if (!user) {
+            return res.status(404).json({
+                message: "User not found",
+                success: false
+            })
+        }
+
+        if (bio) user.bio = bio;
+        if (gender) user.gender = gender;
+        if (profilePicture) user.profilePicture = cloudResponse.secure_url
+
+        await user.save()
+        return res.status(200).json({
+            message: "Profile Updated",
+            success: true,
+            user
+        })
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+export const getSuggestUser = async (req, res) => {
+    try {
+        const suggestedUsers = await User.find({ _id: { $ne: req.id } }).select("-password");
+        if (!suggestedUsers) {
+            return res.status(400).json({
+                message: 'Currently do not have any users',
+                success: false
+            })
+        }
+        return res.status(200).json({
+            message: 'users retrieved successfully',
+            success: true,
+            users: suggestedUsers
+        })
+
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+export const followOrUnfollow = async (req, res) => {
+    try {
+        const myId = req.id;
+        const followingId = req.params.id;
+        if (myId === followingId) {
+            return res.status(400).json({
+                message: 'You cannot follow/unfollow youself',
+                success: false
+            })
+        }
+        const user = await User.findById(myId);
+        const targetUser = await User.findById(followingId)
+        if (!user || !targetUser) {
+            return res.status(400).json({
+                message: 'User not found',
+                success: false
+            })
+        }
+
+        const isFollowing = user.following.includes(followingId);
+        if (isFollowing) {
+            //    unfollow
+            await Promise.all([
+                User.updateOne({ _id: myId }, { $pull: { following: followingId } }),
+                User.updateOne({ _id: followingId }, { $pull: { followers: myId } })
+            ])
+            return res.status(200).josn({
+                message: 'Unfollow successfully',
+                success: true
+            })
+        } else {
+            //  follow
+            await Promise.all([
+                User.updateOne({ _id: myId }, { $push: { following: followingId } }),
+                User.updateOne({ _id: followingId }, { $push: { followers: myId } })
+            ])
+            return res.status(200).josn({
+                message: 'followed successfully',
+                success: true
+            })
+        }
+
+    } catch (error) {
+        console.log(error);
+    }
+}
